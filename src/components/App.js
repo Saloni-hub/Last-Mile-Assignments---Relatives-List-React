83c49b151af5738be44b256e43ca5fe261af07ca
Saloni Pandey <70425604+Saloni-hub@users.noreply.github.com>
2021-08-01 20:27:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
+class App extends React.Component {
     render() {
-
+        const relativeList1 = [
+            {Name: 'Saloni', key: 'relativeListItem1'},
+            {Name: 'Riya', key: 'relativeListItem2'},
+            {Name: 'Juhi', Key: 'relativeListItem3'},
+            {Name: 'Priyanshi', key: 'relativeListItem4'}
+        ];
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               {/* Do not remove the main divn */}
+               <ol key='relativeList'> 
+                   {relativeList1.map(relative => {
+                       return (
+                       <li key={relative.key}>
+                         {relative.Name}
+                       </li>
+                       );
+                   })}
+               </ol>
             </div>
-        )
+        );
     }
 }
 
